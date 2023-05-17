8cb6200125ecf034340dd913eaf49ff9ff1faaf2
tanawat wirattangsakul <tanawat.wir@cp-origin.com>
2023-05-17 15:21:03 +0700

    fix: update constants color

--- constants/space.js
+++ constants/space.js
@@ -2,19 +2,19 @@ export const SPACE_STATUS = {
   vacant: {
     value: "vacant",
     label: "ว่าง",
-    color: (theme) => theme.palette.success.main,
+    color: "#F65050",
     background: "rgba(0, 167, 67, 0.2)",
   },
   occupied: {
     value: "occupied",
     label: "ไม่ว่าง",
-    color: (theme) => theme.palette.error.main,
+    color: "#00A743",
     background: "rgba(246, 80, 80, 0.2)",
   },
   temp: {
     value: "temp",
     label: "temp",
     color: (theme) => theme.palette.warning.main,
-    background: "rgba(246, 210, 80, 0.2)",
+    background: "#F6D250",
   },
 };
