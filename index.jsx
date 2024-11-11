9cc368bc7c83cf2b37cb6317cf0888269d661cae
Developer Army <tanawat.wir@cp-origin.com>
2024-11-11 17:33:25 +0700

    feat: create new layers props

--- index.jsx
+++ index.jsx
@@ -10,86 +10,86 @@ import componentStore from "./stores/component.store";
 import FeatureLeaflet from "./features";
 
 const Leaflet = memo(function Component(props) {
-  useEffect(() => {
-    polygonStore.setState({ statusColor: props.polygonColor });
-    markerStore.setState({ statusColor: props.markerColor });
-    componentStore.setState(props.components);
-  }, [props.polygonColor, props.markerColor, props.components]);
+	useEffect(() => {
+		polygonStore.setState({ statusColor: props.polygonColor });
+		markerStore.setState({ statusColor: props.markerColor });
+		componentStore.setState(props.components);
+	}, [props.polygonColor, props.markerColor, props.components]);
 
-  return (
-    <Box
-      id="markers-canvas-container"
-      sx={{
-        textAlign: "center",
-        "& .leaflet-container": {
-          height: props.height ?? 600,
-        },
-        "& .leaflet-draw-actions": {
-          display: "none !important",
-        },
-        "& .leaflet-draw-section": {
-          display: "none",
-        },
-        "& .leaflet-div-icon": {
-          height: "12px !important",
-          width: "12px !important",
-        },
-        "& .leaflet-container .leaflet-control-attribution": {
-          display: "none",
-        },
-      }}
-    >
-      <Box
-        alt="floorplan-image"
-        component={MapContainer}
-        zoom={1}
-        maxZoom={5}
-        sx={{
-          bgcolor: "white",
-        }}
-        maxBounds={[
-          [-100, -220],
-          [100, 220],
-        ]}
-        boundsOptions={{ padding: 0 }}
-        center={[51.505, -0.09]}
-      >
-        <TileLayer
-          attribution="meow"
-          noWrap
-          url={`${props.url}/{z}/{x}/{y}.png`}
-        />
-        <InitMapStore />
-        <FeatureLeaflet />
-      </Box>
-    </Box>
-  );
+	return (
+		<Box
+			id="markers-canvas-container"
+			sx={{
+				textAlign: "center",
+				"& .leaflet-container": {
+					height: props.height ?? 600,
+				},
+				"& .leaflet-draw-actions": {
+					display: "none !important",
+				},
+				"& .leaflet-draw-section": {
+					display: "none",
+				},
+				"& .leaflet-div-icon": {
+					height: "12px !important",
+					width: "12px !important",
+				},
+				"& .leaflet-container .leaflet-control-attribution": {
+					display: "none",
+				},
+			}}
+		>
+			<Box
+				alt="floorplan-image"
+				component={MapContainer}
+				zoom={1}
+				maxZoom={5}
+				sx={{
+					bgcolor: "white",
+				}}
+				maxBounds={[
+					[-100, -220],
+					[100, 220],
+				]}
+				boundsOptions={{ padding: 0 }}
+				center={[51.505, -0.09]}
+			>
+				<TileLayer
+					attribution="meow"
+					noWrap
+					url={`${props.url}/{z}/{x}/{y}.png`}
+				/>
+				<InitMapStore />
+				<FeatureLeaflet layers={props?.layers} />
+			</Box>
+		</Box>
+	);
 });
 
 function InitMapStore() {
-  const map = useMap();
+	const map = useMap();
 
-  const [setMap, setZoomAmplified] = floorPlanStore((e) => [
-    e.setMap,
-    e.setZoomAmplified,
-  ]);
+	const [setMap, setZoomAmplified] = floorPlanStore((e) => [
+		e.setMap,
+		e.setZoomAmplified,
+	]);
 
-  const mapEvents = useMapEvents({
-    zoomend: () => setZoomAmplified(mapEvents.getZoom()),
-  });
+	const mapEvents = useMapEvents({
+		zoomend: () => setZoomAmplified(mapEvents.getZoom()),
+	});
 
-  useEffect(() => {
-    if (map) {
-      setMap(map);
-    }
+	useEffect(() => {
+		if (map) {
+			setMap(map);
+		}
 
-    return () => {
-      console.log("unmount");
-      setZoomAmplified(1);
-    };
-  }, [map]);
+		return () => {
+			console.log("unmount");
+			setZoomAmplified(1);
+		};
+	}, [map]);
 
-  return <></>;
+	return <></>;
 }
 
 export { floorPlanStore, markerStore, polygonStore };
