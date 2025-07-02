29e1e4a03c8bf3bcb85724d96652ee26d2c6287d
Zonxpk <pakawat.zon@gmail.com>
2025-07-02 16:18:57 +0700

    refactor: improve formatting and add unique IDs for tooltip panes in CustomPolygon component

--- polygons/polygon.tsx
+++ polygons/polygon.tsx
@@ -9,15 +9,16 @@ import componentStore from "../stores/component.store";
 import floorPlanStore from "../stores/floor-plan.store";
 import polygonStore from "../stores/polygon.store";
 import {
-  calculateCenterOfPositions,
-  calculatePolybelOfPositions,
+	calculateCenterOfPositions,
+	calculatePolybelOfPositions,
 } from "../utils/leaflet";
 import { IBaseComponentProps, ILayer, IPosition } from "../types/common";
 import { Path } from "leaflet";
+import { nanoid } from "nanoid";
 
 interface CustomPolygonProps extends IBaseComponentProps {
-  layer: ILayer;
-  selectedLayer?: boolean;
+	layer: ILayer;
+	selectedLayer?: boolean;
 }
 
 /**
@@ -27,89 +28,89 @@ interface CustomPolygonProps extends IBaseComponentProps {
  * @returns Polygon component with appropriate editing controls
  */
 const CustomPolygon: FC<CustomPolygonProps> = (props) => {
-  const { layer, selectedLayer } = props;
-  const polygonRef = useRef<Path>(null);
+	const { layer, selectedLayer } = props;
+	const polygonRef = useRef<Path>(null);
 
-  const statusColor = polygonStore((e) => e.statusColor);
-  const disabledMarker = floorPlanStore(
-    (e) => e.mode === "edit" && selectedLayer
-  );
-  const mode = floorPlanStore((e) => e.mode);
+	const statusColor = polygonStore((e) => e.statusColor);
+	const disabledMarker = floorPlanStore(
+		(e) => e.mode === "edit" && selectedLayer,
+	);
+	const mode = floorPlanStore((e) => e.mode);
 
-  const { Tooltip, Label, PolygonMarker, polygonHandleClick, polygonProps } =
-    componentStore();
+	const { Tooltip, Label, PolygonMarker, polygonHandleClick, polygonProps } =
+		componentStore();
 
-  const status = props.layer.status;
+	const status = props.layer.status;
 
-  // Calculate the center of the polygon for positioning related elements
-  const center = useMemo(
-    () =>
-      layer.position_data?.[0] && layer.position_data.length === 4
-        ? calculateCenterOfPositions(layer.position_data)
-        : calculatePolybelOfPositions(layer.position_data || []),
-    [layer.position_data]
-  );
+	// Calculate the center of the polygon for positioning related elements
+	const center = useMemo(
+		() =>
+			layer.position_data?.[0] && layer.position_data.length === 4
+				? calculateCenterOfPositions(layer.position_data)
+				: calculatePolybelOfPositions(layer.position_data || []),
+		[layer.position_data],
+	);
 
-  const childrenProps = useMemo(
-    () => ({ ...layer, center, status }),
-    [layer, center, status]
-  );
+	const childrenProps = useMemo(
+		() => ({ ...layer, center, status }),
+		[layer, center, status],
+	);
 
-  // Enable editing for selected polygons in edit mode
-  useEffect(() => {
-    if (polygonRef.current && props?.selectedLayer) {
-      // @ts-ignore
-      polygonRef.current?.editing?.enable?.();
-    }
-    if (mode !== "edit") {
-      // @ts-ignore
-      polygonRef.current?.editing?.disable?.();
-    }
-  }, [props?.selectedLayer, mode]);
+	// Enable editing for selected polygons in edit mode
+	useEffect(() => {
+		if (polygonRef.current && props?.selectedLayer) {
+			// @ts-ignore
+			polygonRef.current?.editing?.enable?.();
+		}
+		if (mode !== "edit") {
+			// @ts-ignore
+			polygonRef.current?.editing?.disable?.();
+		}
+	}, [props?.selectedLayer, mode]);
 
-  return (
-    <CustomEditControl disabled={!selectedLayer}>
-      <Polygon
-        ref={polygonRef as any}
-        key={`${layer.id}-${layer?.category ?? ""}`}
-        attribution={String(layer.id)}
-        color="#2b2b2b"
-        fillColor={statusColor?.[layer?.category ?? ""]?.color}
-        pathOptions={{
-          fillOpacity: 0.85,
-        }}
-        eventHandlers={{
-          click: () => polygonHandleClick(layer.id),
-        }}
-        positions={map(
-          layer.position_data || [],
-          (position: IPosition) =>
-            [position.lat, position.lng] as [number, number]
-        )}
-        {...polygonProps({ layer, status, center })}
-      >
-        <Pane name={`tooltip-pane-${props?.layer?.id}`}>
-          <Tooltip {...childrenProps} />
-        </Pane>
-        {!disabledMarker && (
-          <Pane name={`polygon-marker-pane-${props?.layer?.id}`}>
-            <PolygonMarker
-              eventHandlers={{
-                click: () => polygonHandleClick(layer.id),
-              }}
-              {...childrenProps}
-            >
-              <Pane name={`tooltip-inner-pane-${props?.layer?.id}`}>
-                {/* fix cant set eventsPointer: none */}
-                <Tooltip {...childrenProps} />
-              </Pane>
-            </PolygonMarker>
-            <Label {...childrenProps} />
-          </Pane>
-        )}
-      </Polygon>
-    </CustomEditControl>
-  );
+	return (
+		<CustomEditControl disabled={!selectedLayer}>
+			<Polygon
+				ref={polygonRef as any}
+				key={`${layer.id}-${layer?.category ?? ""}`}
+				attribution={String(layer.id)}
+				color="#2b2b2b"
+				fillColor={statusColor?.[layer?.category ?? ""]?.color}
+				pathOptions={{
+					fillOpacity: 0.85,
+				}}
+				eventHandlers={{
+					click: () => polygonHandleClick(layer.id),
+				}}
+				positions={map(
+					layer.position_data || [],
+					(position: IPosition) =>
+						[position.lat, position.lng] as [number, number],
+				)}
+				{...polygonProps({ layer, status, center })}
+			>
+				<Pane name={`tooltip-pane-${nanoid()}`}>
+					<Tooltip {...childrenProps} />
+				</Pane>
+				{!disabledMarker && (
+					<Pane name={`polygon-marker-pane-${nanoid()}`}>
+						<PolygonMarker
+							eventHandlers={{
+								click: () => polygonHandleClick(layer.id),
+							}}
+							{...childrenProps}
+						>
+							<Pane name={`tooltip-inner-pane-${nanoid()}`}>
+								{/* fix cant set eventsPointer: none */}
+								<Tooltip {...childrenProps} />
+							</Pane>
+						</PolygonMarker>
+						<Label {...childrenProps} />
+					</Pane>
+				)}
+			</Polygon>
+		</CustomEditControl>
+	);
 };
 
 export default CustomPolygon;
